044c3b16a779696824f28a752951728b40c155a1
halmi.hodzhov <halmi.hodzhov@egt-digital.com>
2022-05-17 23:43:38 +0300

    wip: configure flyway

--- src/main/java/nbu/logistic/company/Application.java
+++ src/main/java/nbu/logistic/company/Application.java
@@ -25,26 +25,26 @@ public class Application {
 	}
 
 	//executes after application starts
-	@Bean
-	CommandLineRunner run (UserService userService) {
-		return args -> {
-			userService.saveRole(new Role(null, "ADMIN"));
-			userService.saveRole(new Role(null, "AGENT"));
-			userService.saveRole(new Role(null, "COURIER"));
-			userService.saveRole(new Role(null, "CLIENT"));
-
-			userService.saveUser(new ApiUser(null, "John Travolta", "john", "1234", new ArrayList<>()));
-			userService.saveUser(new ApiUser(null, "Will Smith", "will", "1234", new ArrayList<>()));
-			userService.saveUser(new ApiUser(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
-			userService.saveUser(new ApiUser(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
-
-			userService.addRoleToUser("john", "ROLE_USER");
-			userService.addRoleToUser("john", "ROLE_MANAGER");
-			userService.addRoleToUser("will", "ROLE_MANAGER");
-			userService.addRoleToUser("jim", "ROLE_ADMIN");
-			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
-			userService.addRoleToUser("arnold", "ROLE_ADMIN");
-			userService.addRoleToUser("arnold", "ROLE_USER");
-		};
-	}
+//	@Bean
+//	CommandLineRunner run (UserService userService) {
+//		return args -> {
+//			userService.saveRole(new Role(null, "ADMIN"));
+//			userService.saveRole(new Role(null, "AGENT"));
+//			userService.saveRole(new Role(null, "COURIER"));
+//			userService.saveRole(new Role(null, "CLIENT"));
+//
+//			userService.saveUser(new ApiUser(null, "John Travolta", "john", "1234", new ArrayList<>()));
+//			userService.saveUser(new ApiUser(null, "Will Smith", "will", "1234", new ArrayList<>()));
+//			userService.saveUser(new ApiUser(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+//			userService.saveUser(new ApiUser(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+//
+//			userService.addRoleToUser("john", "ROLE_USER");
+//			userService.addRoleToUser("john", "ROLE_MANAGER");
+//			userService.addRoleToUser("will", "ROLE_MANAGER");
+//			userService.addRoleToUser("jim", "ROLE_ADMIN");
+//			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+//			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+//			userService.addRoleToUser("arnold", "ROLE_USER");
+//		};
+//	}
 }

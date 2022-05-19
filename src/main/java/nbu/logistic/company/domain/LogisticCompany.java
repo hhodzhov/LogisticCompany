02a83928fa8aeb448193fb0309e713f5282ad58c
halmi.hodzhov <halmi.hodzhov@egt-digital.com>
2022-05-19 19:34:55 +0300

    add relationship between logistic company and office

--- src/main/java/nbu/logistic/company/domain/LogisticCompany.java
+++ src/main/java/nbu/logistic/company/domain/LogisticCompany.java
@@ -9,26 +9,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
-@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Office {
+@NoArgsConstructor
+public class LogisticCompany {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    String name;
+
+    String country;
+
     String city;
 
-    String officeName;
+    String centralOfficeAddress;
 
-    String address;
-
-    @OneToOne
-    LogisticCompany logisticCompany;
+    @OneToMany
+    List<Office> offices = new ArrayList<>();
 
 }
--- src/main/resources/db/migration/V3__create_logistic_company_table.sql
+++ src/main/resources/db/migration/V3__create_logistic_company_table.sql
@@ -9,3 +9,14 @@ create table "nbu-project".logistic_company
 
 insert into "nbu-project".logistic_company
 values (1, 'NBU LOGISTICS', 'Bulgaria', 'Sofia', 'zhk. Ovcha Kupel 2');
+
+alter table office
+    add column logistic_company_id int4;
+
+alter table office
+    add constraint FK413ijhlekjh6ijeyg32pijsvb
+        foreign key (logistic_company_id)
+            references logistic_company;
+
+update office
+set logistic_company_id = 1;
